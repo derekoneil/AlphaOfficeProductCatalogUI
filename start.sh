6226ac7dd65481df59a1a4384647741c743ca9a9
Derek O'Neill <derekoneil@gmail.com>
2017-12-06 09:37:00 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 /usr/local/glassfish4/bin/asadmin start-domain
-/usr/local/glassfish4/bin/asadmin -u admin deploy /alpha.war
+/usr/local/glassfish4/bin/asadmin -u admin deploy target/AlphaProducts-0.0.1-SNAPSHOT.war
 /usr/local/glassfish4/bin/asadmin stop-domain
 /usr/local/glassfish4/bin/asadmin start-domain --verbose
